1dae4ae69d9e05ee4ac546985ee5abb5b609a1c9
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-04-01 01:13:05 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>anshu51379</h1>
+                <p>@2023 abhishek kumar</p>
+            </div>
+
+            <div>
+                <h5>Follow Us!</h5>
+                <div>
+                    <a href="https://github.com/anshu51379" target={"blank"}>
+                        Github
+                    </a>
+                    <a href="https://instagram.com/anshu51379" target={"blank"}>
+                        Instagram
+                    </a>
+                    <a href="https://youtube.com/AbhishekKumar51379" target={"blank"}>
+                        Youtube
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
